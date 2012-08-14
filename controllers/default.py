ee428a8b6b96765acc86a169ccce5579f5afb8d8
Emir Salazar <pooky03@gmail.com>
2012-08-14 16:39:44 -0500

    v0.1
    se tiene completa la vista de inicio de sesión,

--- controllers/default.py
+++ controllers/default.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 
-def index():
-    form = SQLFORM(db.language)
-    return dict(form=form)
 
-def error():
-    return dict()
+def index():
+	form = FORM(
+				FIELDSET(
+						INPUT(_id='usr', _type='text', _placeholder='Usuario', _required='required'),
+						INPUT(_id='pwd', _type='password', _placeholder='Contraseña', _required='required')
+				,_id='inputs'),
+				FIELDSET(
+						INPUT(_type='submit', _id='submit', _value='Entrar')
+				,_id='actions')
+			)
+
+	if form.accepts(request,session):
+		response.flash = 'form accepted'
+	return dict(form=form)
+
+def data():
+	return '0'
 
--- models/language.py
+++ models/language.py
@@ -1,24 +1,3 @@
-# -*- coding: utf-8 -*-
-from datetime import datetime
-
-db = DAL('postgres://beardcode:qazWSX11@localhost/fayol')
-
-now = datetime.now()
-
-db.define_table('language',
-    Field('name', 'string', required=True, notnull=True),
-    Field('code', 'string'),
-    Field('locale', 'string'),
-    Field('image', 'string'),
-    Field('directory', 'string'),
-    Field('filename', 'string'),
-    Field('sort_order', 'integer', default=1),
-    Field('status', 'boolean', default=True),
-    Field('date_added', 'datetime'),
-    Field('date_modified', 'datetime', default=now),
-    migrate=False
-)
-
 
 
 
