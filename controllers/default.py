7d718a0232a198d8c0bd6a481fcc4f6499dd28f5
Emir Salazar <pooky03@gmail.com>
2012-08-15 18:01:23 -0500

    pwd focus fixed

--- controllers/default.py
+++ controllers/default.py
@@ -5,7 +5,7 @@ def index():
 	form = FORM(
 				FIELDSET(
 						INPUT(_id="usr", _name='usr', _class='input_usr', _type='text', _placeholder='Usuario', _required='required'),
-						INPUT(_id="pwd", _name='pwd',_class='input_pwd', _type='password', _placeholder='Contraseña', _required='required')
+						INPUT(_id="pwd", _name='pwd', _class='input_pwd', _type='password', _placeholder='Contraseña', _required='required')
 				,_id='inputs'),
 				FIELDSET(
 						INPUT(_type='submit', _id='submit', _value='Entrar')
