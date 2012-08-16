91073da8e633fb460c2687b3c14bf042547167bc
Juan Diego Romero González <jd@beardcode.mx>
2012-08-16 12:39:21 -0500

    Partial token validation.

--- controllers/default.py
+++ controllers/default.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
+from gluon.utils import web2py_uuid
 
 
 def index():
-	form = FORM(
-				FIELDSET(
-						INPUT(_id="usr", _name='usr', _class='input_usr', _type='text', _placeholder='Usuario', _required='required'),
-						INPUT(_id="pwd", _name='pwd',_class='input_pwd', _type='password', _placeholder='Contraseña', _required='required')
-				,_id='inputs'),
-				FIELDSET(
-						INPUT(_type='submit', _id='submit', _value='Entrar')
-				,_id='actions')
-			)
 
-	if form.accepts(request,session):
-		response.flash = 'form accepted'
-	#tkn = form.vars.usr = "mac"
-	return dict(form=form)
+    tkn = web2py_uuid()
+    session.tkn = tkn
 
-def data():
-	return '0'
+    form = FORM(
+        FIELDSET(
+            INPUT(_id="usr", _name='usr', _class='input_usr', _type='text', _placeholder='Usuario', _required='required'),
+            INPUT(_id="pwd", _name='pwd',_class='input_pwd', _type='password', _placeholder='Contraseña', _required='required'),
+            _id='inputs'
+        ),
+        FIELDSET(
+            INPUT(_type='submit', _id='submit', _value='Entrar'),
+            INPUT(_type='hidden', _id='tkn', _name='tkn', value='%s' % tkn),
+            _id='actions'
+        )
+    )
 
+    return dict(form=form)
