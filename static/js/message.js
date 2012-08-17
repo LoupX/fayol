784e097cd40c228d244a6f55da2a32d62992e959
Emir Salazar <pooky03@gmail.com>
2012-08-17 11:32:59 -0500

    improvements in notification's system

--- static/js/message.js
+++ static/js/message.js
@@ -1,24 +1,37 @@
-//<div id="fondo">
-//    <div id="mensaje" class="grid_4 text-center">
-//    Error<br>Contrasena o usuario incorrecto.<br><br>
-//        <input id="btn" type="button" style="width:90%;" value="Aceptar">
-//        </div>
-//    </div>
-//
-function message_show(h,w) {
+/*
+* Notifications for user's errors
+* append a div into first place of the body and calculate the center of the document
+* message_show receive 2 parameters, the first one is a css class and the next one is the message error.
+*
+*
+*/
+jQuery('body').prepend("<div id='fondo'><div id='mensaje' class='grid_4 text-center'> </div></div>");
 
-	var h = h,
-		w = w;
-	jQuery('#mensaje').css({'margin-left': w, 'margin-top': h});
-    jQuery('#fondo').fadeIn("fast");
-	jQuery("#mensaje")
+var h   = (jQuery(document).height()/2)-100,
+    w   = (jQuery(document).width()/2)-150,
+    men = jQuery('#mensaje'),
+    fon = jQuery('#fondo'),
+    btn = jQuery('#btn');
+
+
+function message_show(c, m) {
+    var m = m,
+        c = c;
+	men.css({'margin-left': w, 'margin-top': h});
+    fon.fadeIn("fast");
+	men
+        .addClass(c)
+        .html('')
+        .prepend(m+"<br><br><input id='btn' type='button' style='width:90%;' value='Aceptar'>")
 		.stop(true)
 		.effect("puff", { mode: 'show', percent: 50 }, 200)
 		.effect("bounce", { mode: 'show', distance: 25, times: 6, direction: 'down'}, 200);
 		//.effect("puff", { mode: 'show', percent: 50 }, 300);
-}
 
-function message_hide() {
-	jQuery('#fondo').fadeOut('fast');
-	jQuery("#mensaje").slideUp( function() {});
+    btn.live('click',function(){
+        fon.fadeOut('fast');
+        men
+            .slideUp( function() {})
+            .removeClass(c);
+    });
 }
