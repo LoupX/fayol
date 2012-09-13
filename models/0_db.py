9371c873ffa39420c74da2cfbcb133b0cbeff3b8
Juan D. Romero <jd@beardcode.mx>
2012-09-12 19:25:10 -0500

    Models repaired There was a problem with the execution order of the models an other sintaxis errors

--- models/0_db.py
+++ models/0_db.py
@@ -2,6 +2,3 @@ try:
     db = DAL('postgres://%(user)s:%(pwd)s@%(host)s:%(port)i/%(db)s' % dict(user=DB_USER, pwd = DB_PASS, host=DB_HOST, port=DB_PORT, db=DB))
 except Exception as e:
     raise HTTP(503)
-
-from gluon.tools import Auth
-auth = Auth(db, hmac_key=Auth.get_or_create_key())
--- models/5_models_users.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-auth.define_tables(username=True)
-
-auth.settings.controller = 'login'
-auth.settings.function = 'index'
-auth.settings.login_url = URL('login', 'index')
-auth.settings.login_next = URL('default', 'index')
-auth.settings.logout_next = URL('login', 'index')
