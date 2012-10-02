8d27f52c986ec0d1c814e0bb392695e179f7a91c
Juan Diego Romero González <jd@beardcode.mx>
2012-10-02 13:03:52 -0500

    Test

--- views/untitled.js
+++
@@ -1,8 +0,0 @@
-function message() {
-	jQuery('#fondo').fadeIn("fast");
-	jQuery("#mensaje")
-		.stop(true)
-		.effect("puff", { mode: 'show', percent: 50 }, 200)
-		.effect("bounce", { mode: 'show', distance: 25, times: 6, direction: 'down'}, 200);
-		//.effect("puff", { mode: 'show', percent: 50 }, 300);
-}
