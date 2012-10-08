0c132d4088dd567f0bd9172cf313b5b39f89dbc1
Juan Diego Romero González <jd@beardcode.mx>
2012-10-08 10:26:52 -0500

    Update vendors.

--- controllers/vendors.py
+++ controllers/vendors.py
@@ -5,7 +5,7 @@ def index():
     title = 'Proveedores'
     return dict(title=title)
 
-def new_vendor():
+def new():
     title = 'Proveedores'
     states = read_states()
     if states:
@@ -17,6 +17,54 @@ def new_vendor():
             _value=0))
 
     return dict(title=title, states=states)
+
+def update():
+    title = 'Proveedores'
+    vendor_id = None
+    if request.vars.id:
+        vendor_id = request.vars.id
+    else:
+        redirect(URL(c='vendors', f='new'))
+    row = db(db.vendors.id==vendor_id).select().as_list()
+    row = row[0]
+    if not row:
+        redirect(URL(c='vendors', f='new'))
+
+    options = [OPTION('Seleccionar', _value='')]
+    states = read_states()
+    if states:
+        for s in states:
+            if s['id']==row['state_id']:
+                options += [OPTION(s['name'], _value=s['id'],
+                    _selected='selected')]
+            else:
+                options += [OPTION(s['name'], _value=s['id'])]
+    states = SELECT(_name='state', _id='state', *options)
+
+    options = [OPTION('Seleccionar', _value='')]
+    municipalities = read_municipalities(row['state_id'])
+    if municipalities:
+        for m in municipalities:
+            if m['id']==row['municipality_id']:
+                options += [OPTION(m['name'], _value=m['id'],
+                    _selected='selected')]
+            else:
+                options += [OPTION(m['name'], _value=m['id'])]
+    municipalities = SELECT(_name='municipality', _id='municipality', *options)
+
+    options = [OPTION('Seleccionar', _value='')]
+    localities = read_localities(row['municipality_id'])
+    if localities:
+        for l in localities:
+            if l['id']==row['locality_id']:
+                options += [OPTION(l['name'], _value=l['id'],
+                    _selected='selected')]
+            else:
+                options += [OPTION(l['name'], _value=l['id'])]
+    localities = SELECT(_name='locality', _id='locality', *options)
+    return dict(title=title, states=states, municipalities=municipalities,
+        localities=localities, **row)
+
 
 def contact_information():
     title = 'Proveedores'
