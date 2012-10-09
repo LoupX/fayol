efdd9d36b86210d61c9e3315b3658db8ca42f1a4
Emir Salazar <pooky03@gmail.com>
2012-10-09 16:11:54 -0500

    notification for new vendors, smoth scroll and clean fields.

--- static/js/custom/general.js
+++ static/js/custom/general.js
@@ -204,7 +204,8 @@ jQuery(document).ready(function(){
 	
 	jQuery('.notibar .close').click(function(){
 		jQuery(this).parent().fadeOut(function(){
-			jQuery(this).remove();
+			//jQuery(this).remove();
+			jQuery(this).hide();
 		});
 	});
 	
