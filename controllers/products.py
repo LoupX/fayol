9dcc9387a8f13dabf204f4824e2d8967c21244f2
José Lara <joselara@beardcode.mx>
2012-10-09 16:09:20 -0500

    measuring unit forms

--- controllers/products.py
+++ controllers/products.py
@@ -5,21 +5,31 @@ def index():
     title = 'Productos'
     return dict(title=title)
     
+def new_product():
+	title = 'Productos'
+	return dict(title=title)
     
 def categories():
     title = 'Categorías'
     return dict(title=title)
-    
-    
+     
 def trademarks():
     title = 'Marcas'
     return dict(title=title)
     
-    
 def units():
     title = 'Unidades de medida'
-    return dict(title=title)
-    
-def new_product():
-	title = 'Productos'
+    current = ['menu_catalogs', 'sidebar_units', 'sub_units_read']
+    return dict(title=title, current=current)
+
+def units_quickedit():
+	title = 'Unidades de medida'
 	return dict(title=title)
+	
+def new_unit():
+    title = 'Unidades de medida'
+    current = ['menu_catalogs', 'sidebar_units', 'sub_units_new']
+    return dict(title=title,  current=current)
+    
+
+	
