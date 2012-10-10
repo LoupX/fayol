4e0d5b502be67852a0f3e0f7c3e2d376b365852a
José Lara <joselara@beardcode.mx>
2012-10-09 19:33:42 -0500

    Categories and trademarks forms

--- controllers/products.py
+++ controllers/products.py
@@ -8,14 +8,34 @@ def index():
 def new_product():
 	title = 'Productos'
 	return dict(title=title)
+	     
+def trademarks():
+    title = 'Marcas'
+    current = ['menu_catalogs', 'sidebar_trademark', 'sub_trademark_read']
+    return dict(title=title, current=current)
+    
+def trademark_quickedit():
+	title = 'Marcas'
+	return dict(title=title)
+
+def new_trademark():
+	title = 'Marcas'
+	current = ['menu_catalogs', 'sidebar_trademark', 'sub_trademark_new']
+	return dict(title=title, current=current)    
     
 def categories():
     title = 'Categorías'
-    return dict(title=title)
-     
-def trademarks():
-    title = 'Marcas'
-    return dict(title=title)
+    current = ['menu_catalogs', 'sidebar_categories', 'sub_categories_read']
+    return dict(title=title, current=current)
+    
+def categories_quickedit():
+	title = 'Unidades de medida'
+	return dict(title=title)
+    
+def new_category():
+	title = 'Categorías'
+	current = ['menu_catalogs', 'sidebar_categories', 'sub_categories_new']
+	return dict(title=title, current=current)
     
 def units():
     title = 'Unidades de medida'
@@ -30,6 +50,3 @@ def new_unit():
     title = 'Unidades de medida'
     current = ['menu_catalogs', 'sidebar_units', 'sub_units_new']
     return dict(title=title,  current=current)
-    
-
-	
