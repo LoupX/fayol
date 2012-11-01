662222eff07db5176457f5eab595de121091c404
José Lara <joselara@beardcode.mx>
2012-10-31 19:24:28 -0600

    product

--- controllers/inventory.py
+++ controllers/inventory.py
@@ -2,6 +2,24 @@
 
 @auth.requires_login()
 def index():
-    title = 'Inventarios'
-    current = ['menu_inventory']
+    title = 'Consultar movimientos'
+    current = ['menu_inventory', 'sidebar_index']
+    return dict(title=title, current=current)
+    
+@auth.requires_login()
+def entries():
+    title = 'Entradas'
+    current = ['menu_inventory', 'sidebar_entries']
+    return dict(title=title, current=current)
+
+@auth.requires_login()
+def discounts():
+    title = 'Salidas'
+    current = ['menu_inventory', 'sidebar_discounts']
+    return dict(title=title, current=current)
+    
+@auth.requires_login()
+def tranfers():
+    title = 'Traspasos'
+    current = ['menu_inventory', 'sidebar_tranfers']
     return dict(title=title, current=current)
