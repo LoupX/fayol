b1c2d1e9556128aacf6e5f600067780b9391ac6e
José Lara <joselara@beardcode.mx>
2012-11-07 13:14:33 -0600

    user

--- controllers/users.py
+++ controllers/users.py
@@ -11,3 +11,9 @@ def new_user():
     title = 'Usuarios'
     current = ['menu_users', 'sidebar_users', 'sub_users_new']
     return dict(title=title, current=current)
+    
+@auth.requires_login()
+def update_user():
+    title = 'Usuarios'
+    current = ['menu_users', 'sidebar_users', 'sub_users_read']
+    return dict(title=title, current=current)
