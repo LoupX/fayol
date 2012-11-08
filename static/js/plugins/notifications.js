769c715a6734259c5b1ea98b96ee1bf90074aa7f
Emir Salazar <pooky03@gmail.com>
2012-11-07 18:18:39 -0600

    improved in notifications

--- static/js/plugins/notifications.js
+++ static/js/plugins/notifications.js
@@ -1,10 +1,16 @@
 var Messages = {
-    merror: "something was wrong",
-    msuccess: "something was right",
-    mexists: "something exists",
-    malert: "be careful",
+    merror: "Ocurrió un error con los datos del formulario.",
+    msuccess: "Se han guardado los datos del formulario.",
+    mexists: "Ese proveedor existe, para editarlo vaya a la sección de ver proveedor.",
+    malert: "Verifique los datos antes de guardar, posteriormente no se podran editar.",
+    nspeed: 600,
+    sspeed: 1700,
+    scroll: 1000,
     showM: function(which, phrase, speed) {
             speed = (speed) ? speed: 600;
+
+            phrase = this.whichMessage(which, phrase);
+
             jQuery(which+' p').html(phrase);
             jQuery(which).fadeIn(speed);
             this.scrollPage();
@@ -20,20 +26,41 @@ var Messages = {
         jQuery('.msgalert p').html(this.malert);
     },
     scrollPage: function() {
-        jQuery('body','html', window, document).animate( {scrollTop: 0}, 1000);
+        jQuery('body','html', window, document).animate( {scrollTop: 0}, this.scroll);
     }
     ,
     hideAll: function() {
         jQuery('.msgerror, .msgsuccess, .exists').hide();
     }
     ,
-    successWR: function(which, phrase, speed, url) {
-        speed = (speed) ? speed: 1600;
+    successWR: function(which, url, speed, phrase) {
+        speed = (speed) ? speed: this.sspeed;
+
+        phrase = this.whichMessage(which, phrase);
+
         jQuery(which+' p').html(phrase);
         jQuery(which).fadeIn(speed, function() {
             window.location.replace(url);
         });
         this.scrollPage();
     },
-    version: 'v0.1'
+    whichMessage: function(which, phrase) {
+        switch(which) {
+                case '.msgsuccess':
+                    phrase = (phrase) ? phrase: this.msuccess;
+                    break;
+                case '.msgerror':
+                    phrase = (phrase) ? phrase: this.merror;
+                    break;
+                case '.exists':
+                    phrase = (phrase) ? phrase: this.mexists;
+                    break;
+                case '.msgalert':
+                    phrase = (phrase) ? phrase: this.malert;
+                    break;
+        }
+
+        return phrase;
+    },
+    version: 'v0.1.1'
 }
