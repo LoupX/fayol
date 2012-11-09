a8655f791800bce69f542f91c6f1d13b997ed709
José Lara <joselara@beardcode.mx>
2012-11-08 19:04:28 -0600

    clients

--- controllers/clients.py
+++ controllers/clients.py
@@ -11,3 +11,9 @@ def new_client():
     title = 'Clientes'
     current = ['menu_catalogs', 'sidebar_clients', 'sub_clients_new']
     return dict(title=title, current=current)
+    
+@auth.requires_membership('GOD')
+def update_client():
+    title = 'Clientes'
+    current = ['menu_catalogs', 'sidebar_clients', 'sub_clients_read']
+    return dict(title=title, current=current)
